ba37a92e7491aef1c3bf2c2767a063d400e939ae
Anushkagupta07 <anushka.gupta.alwar@gmail.com>
2026-01-08 18:12:47 +0530

    corrections in solution of two_sum

--- ArrayDSA/two_sum.cpp
+++ ArrayDSA/two_sum.cpp
@@ -3,17 +3,14 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         unordered_map<int,int>m; // O(n)
         int n = nums.size();
-        vector<int>ans;
         for(int i=0;i<n;i++){
             int first = nums[i];
             int second = target - first;
             if(m.find(second)!=m.end()){    //m.end()is a special iterator that represents "not found" in a map.
-                ans.push_back(i);
-                ans.push_back(m[second]);
-                break;
+                return {i,m[second]};
             }
             m[first] =i; //It is saving the current number and the index where we saw it into the map m.
         }
-        return ans;
+        return {};
     }
 };
